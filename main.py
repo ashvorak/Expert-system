fba0fe49c00b42f9add9e643964c3d677e8ced07
aleksej <aleksey.shvorak@gmail.com>
2019-10-29 23:12:49 +0200

    expert (beta)

--- main.py
+++ main.py
@@ -13,13 +13,31 @@ class Expert:
         self.knowledge_base = None
         self.results = []
 
+    def check_right_part(self, rules):
+        result = list()
+        for rule in rules:
+            if isinstance(rule[-1], list):
+                value = rule[:-1]
+                parts = ''.join(rule[-1]).split('+')
+                for part in parts:
+                    result.append(value + list(part))
+            else:
+                result.append(rule)
+        return result
+
     def convert_rules(self, rules):
         tmp = dict()
+        rules = self.check_right_part(rules)
         for rule in rules:
-            if tmp.get(rule[-1]):
-                value = tmp[rule[-1]]
-                tmp[rule[-1]] = value.append(tmp[rule[:-2]])
-            tmp[rule[-1]] = rule[:-2]
+            if isinstance(rule[-1], list):
+                fact = ''.join(rule[-1])
+            else:
+                fact = rule[-1]
+            if tmp.get(fact):
+                value = tmp[fact]
+                tmp[fact] = [list(value), list(rule[:-2])]
+            else:
+                tmp[fact] = rule[:-2]
 
         return tmp
 
@@ -50,7 +68,15 @@ class Expert:
             if item in self.trues:
                 print(f'{item} in trues = True')
             elif res.get(item):
-                print(f'{item} in calculation = {res[item].calculate(self.trues, res)}')
+                if isinstance(res[item], list) and len(res[item]) > 1 and \
+                        all([True if not isinstance(i, str) else False for i in res[item]]):
+                    for i in res[item]:
+                        fact = i.calculate(self.trues, res)
+                        if fact:
+                            break
+                else:
+                    fact = res[item].calculate(self.trues, res)
+                print(f'{item} in calculation = {fact}')
             else:
                 print(f'{item} in else = False')
 
@@ -75,9 +101,9 @@ def check_negative_elem(elem):
 
 def recursive(elem):
     if not isinstance(elem, list):
-        return Element(check_negative_elem(elem))
+        return check_negative_elem(elem)
     elif len(elem) == 1:
-        return Element(check_negative_elem(elem[0]))
+        return check_negative_elem(elem[0])
     if elem[1] == '+':
         return And(recursive(elem[0]), recursive(elem[2]))
     elif elem[1] == '|':
@@ -85,7 +111,10 @@ def recursive(elem):
     elif elem[1] == '^':
         return Xor(recursive(elem[0]), recursive(elem[2]))
     else:
-        recursive(elem)
+        data = []
+        for el in elem:
+            data.append(recursive(el))
+        return data
 
 
 def create_classes(rules):
@@ -98,7 +127,7 @@ def create_classes(rules):
     return converted_rules
 
 
-class Element():
+class Element:
 
     def __init__(self, element):
         self.element = element
@@ -110,10 +139,10 @@ class Element():
         return self.element.__str__()
 
     def calculate(self, trues, res):
-        if self.element.element in trues:
+        if self.element in trues:
             return True
-        elif res.get(self.element.element):
-            return res[self.element.element].calculate(trues, res)
+        elif res.get(self.element):
+            return res[self.element].calculate(trues, res)
         else:
             return False
 
@@ -168,6 +197,15 @@ class Not:
 
     def __repr__(self):
         return f'!{self.element.__repr__()}'
+
+    def calculate(self, trues, res):
+        if self.element.element in trues:
+            res = True
+        elif res.get(self.element.element):
+            res = res[self.element.element].calculate(trues, res)
+        else:
+            res = False
+        return not res
 
 
 def main():
